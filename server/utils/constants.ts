4c80d42357b6041212a3f10b5214e0e7eadb11f3
Wei <5h3ll3x@gmail.com>
2020-05-19 02:59:30 +0900

    feat: load keystore from file

--- server/utils/constants.ts
+++ server/utils/constants.ts
@@ -1,4 +1,5 @@
 import { Mixin } from 'mixin-node-sdk'
+import fs from 'fs'
 
 export const MIXIN_API_HOST = 'https://mixin-api.zeromesh.net/'
 
@@ -8,8 +9,11 @@ export const FOX_ONE_API_HOST = 'https://api1.kumiclub.com/api/v2/'
 
 export const FOX_OAUTH_HOST = 'https://oauth2.kumiclub.com/'
 
+const data = fs.readFileSync(process.env.MIXIN_CLIENT_CONFIG, {
+  encoding: 'UTF-8',
+})
 export const mixin = new Mixin(
-  Object.assign(JSON.parse(process.env.MIXIN_CLIENT_CONFIG), {
+  Object.assign(JSON.parse(data), {
     client_secret: process.env.MIXIN_CLIENT_SECRET,
   }),
 )
